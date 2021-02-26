ba627413f06e39048208aca68b13c5ab83997da7
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-26 20:16:19 +0200

    Initial Commit

---
+++ AdvancedTextFieldSwiftUI/AdvancedTextFieldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedTextFieldSwiftUIApp.swift
+//  AdvancedTextFieldSwiftUI
+//
+//  Created by Ramill Ibragimov on 26.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedTextFieldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
