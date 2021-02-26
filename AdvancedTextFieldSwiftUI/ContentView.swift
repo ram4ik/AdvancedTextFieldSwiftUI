3ee9c78e15c5e2cde58fe5319d20a37cc4ee79b5
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-26 21:12:41 +0200

    hideKeyboard fix

--- AdvancedTextFieldSwiftUI/ContentView.swift
+++ AdvancedTextFieldSwiftUI/ContentView.swift
@@ -75,7 +75,7 @@ extension View {
 
 extension View {
     func hideKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.becomeFirstResponder), to: nil, from: nil, for: nil)
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
